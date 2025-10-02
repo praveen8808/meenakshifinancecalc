d5bfbad716d250d7982a3e358bfba8c954c9a3df
Praveen VB <30691175+praveen8808@users.noreply.github.com>
2025-10-02 12:32:32 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'meenakshi-ghp-v1';
+const OFFLINE_URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-1024.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => {
+      const fetchPromise = fetch(req).then(networkRes => {
+        if (networkRes && networkRes.status === 200 && req.method === 'GET') {
+          caches.open(CACHE_NAME).then(cache => cache.put(req, networkRes.clone()));
+        }
+        return networkRes;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
